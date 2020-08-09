b82f66e4479f9723a74637071bcfef47dd1ee961
biubiu <biubiugod@gmail.com>
2020-08-09 09:53:09 -0400

    session

--- framework/hibernate/src/com/how2java/test/TestHibernate.java
+++ framework/hibernate/src/com/how2java/test/TestHibernate.java
@@ -1,29 +1,29 @@
 package com.how2java.test;
  
 import org.hibernate.Session;
+import java.util.HashSet;
+import java.util.Set;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
  
 import com.how2java.pojo.Product;
+import com.how2java.pojo.User;
+import com.how2java.pojo.Category;
  
 public class TestHibernate {
-    public static void main(String[] args) {
- 
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
- 
-        Session s = sf.openSession();
-        s.beginTransaction();
- 
-        for(int i = 0; i <10;i++) {
-        	Product p = new Product();
-        	p.setName("iphone" + i);
-        	p.setPrice(i);
-        	s.save(p);
-        }
-         
-        s.getTransaction().commit();
-        s.close();
-        sf.close();
-    }
- 
-}
+	 public static void main(String[] args) {
+	        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+	   
+	        Session s = sf.openSession();
+	        s.beginTransaction();
+	        System.out.println("log1");
+	        Category c1 = (Category)s.get(Category.class, 1);
+	        System.out.println("log2");
+	        Category c2= (Category)s.get(Category.class, 1);
+	        System.out.println("log3");       
+	 
+	        s.getTransaction().commit();
+	        s.close();
+	        sf.close();
+	    }
+	}
