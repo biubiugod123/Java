e1b9a92f791f049bec72a19aaf8dba5bb308c22d
biubiu <biubiugod@gmail.com>
2020-08-11 05:42:55 -0400

    spring

--- framework/hibernate/src/com/how2java/pojo/Category.java
+++ framework/hibernate/src/com/how2java/pojo/Category.java
@@ -1,26 +1,35 @@
 package com.how2java.pojo;
-import java.util.Set;
 
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+ 
+@Entity
+@Table(name = "category_")
 public class Category {
+    int id;
+    String name;
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
+     
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    int id;
-    String name;
-    Set<Product> products;
-    public Set<Product> getProducts() {
-        return products;
-    }
-    public void setProducts(Set<Product> products) {
-        this.products = products;
-    }
+ 
 }
+ 
--- framework/hibernate/src/com/how2java/pojo/Product.java
+++ framework/hibernate/src/com/how2java/pojo/Product.java
@@ -1,43 +1,52 @@
 package com.how2java.pojo;
 
-import java.util.Set;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "product_")
 public class Product {
     int id;
     String name;
     float price;
     Category category;
-    Set<User> users;
-    
-    public Set<User> getUsers() {
-        return users;
-    }
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category category) {
-        this.category = category;
-    }
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")  
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
+    @Column(name = "name")
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+    @Column(name = "price")
     public float getPrice() {
         return price;
     }
     public void setPrice(float price) {
         this.price = price;
+    }
+    @ManyToOne
+    @JoinColumn(name="cid")
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
     }
       
 }
