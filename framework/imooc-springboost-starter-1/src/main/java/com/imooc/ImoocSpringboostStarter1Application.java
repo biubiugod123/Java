b4f51ab7eff8a0d4203b7a6ae01ef7f78f150b87
biubiu <biubiugod@gmail.com>
2020-08-21 08:26:11 -0400

    springboost

---
+++ framework/imooc-springboost-starter-1/src/main/java/com/imooc/ImoocSpringboostStarter1Application.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSpringboostStarter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSpringboostStarter1Application.class, args);
+	}
+
+}
